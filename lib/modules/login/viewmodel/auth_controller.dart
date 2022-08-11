9e52ae0e92a7214abcd196a5d8d0a75d73602930
Muhammed Salih T <mhdsalih9656@gmail.com>
2022-08-12 01:25:45 +0530

    auth editted

---
+++ lib/modules/login/viewmodel/auth_controller.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login_firebase/models/user_model.dart';
+import 'package:login_firebase/modules/login/view/signin_screen.dart';
+import 'package:login_firebase/routes/routes.dart';
+
+class AuthProvider extends ChangeNotifier {
+  FirebaseAuth fb;
+  AuthProvider(this.fb);
+  bool _isLoading = false;
+
+  Stream<User?> stream() => fb.authStateChanges();
+  bool get loading => _isLoading;
+
+  Future<String> signIn(String email, String password) async {
+    try {
+      _isLoading = true;
+      notifyListeners();
+
+      await fb.signInWithEmailAndPassword(
+          email: email.trim(), password: password.trim());
+
+      _isLoading = false;
+      notifyListeners();
+      return Future.value("");
+    } on FirebaseAuthException catch (ex) {
+      _isLoading = false;
+      notifyListeners();
+      return Future.value(ex.message);
+    }
+  }
+
+  Future<void> logOut(context) async {
+    await fb.signOut();
+    ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("You are successfully logout")));
+    RoutesManager.removeScreen(screen: const SignInScreen());
+  }
+
+  Future<String> signUp(
+      String email, String password, UserModel usermodel) async {
+    try {
+      _isLoading = true;
+      notifyListeners();
+
+      await fb
+          .createUserWithEmailAndPassword(
+              email: email.trim(), password: password.trim())
+          .then((value) => addToFireStore(usermodel));
+
+      _isLoading = false;
+      notifyListeners();
+      return Future.value("");
+    } on FirebaseAuthException catch (ex) {
+      _isLoading = false;
+      notifyListeners();
+      return Future.value(ex.message);
+    }
+  }
+}
+
+Future addToFireStore(UserModel usermodel) async {
+  await FirebaseFirestore.instance
+      .collection("users")
+      .doc(usermodel.uid)
+      .set(usermodel.toJson());
+}
